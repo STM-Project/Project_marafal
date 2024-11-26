4019eb9f2bb0f9326c51cd8e6203df92a29b9b53
STM-Project <51048656+STM-Project@users.noreply.github.com>
2024-11-26 12:03:53 +0100

    Update LCD_fonts_images.h

--- Application/LCD/LCD_fonts_images.h
+++ Application/LCD/LCD_fonts_images.h
@@ -9,6 +9,7 @@
 #define LCD_LCD_FONTS_IMAGES_H_
 
 #include "stm32f7xx_hal.h"
+#include "common.h"
 
 #define MAX_SIZE_TOUCHIDX_FOR_STRVAR		5
 
@@ -321,9 +322,10 @@ StructTxtPxlLen LCD_StrDependOnColorsWindow(uint32_t posBuff,uint32_t BkpSizeX,u
 StructTxtPxlLen LCD_StrDependOnColorsWindowIndirect(uint32_t posBuff, int Xwin, int Ywin,uint32_t BkpSizeX,uint32_t BkpSizeY,int fontID, int Xpos, int Ypos, char *txt, int OnlyDigits, int space, uint32_t bkColor, uint32_t fontColor,uint8_t maxVal, int constWidth);
 uint32_t SetLenTxt2Y(int posY, uint16_t lenTxt);
 
-uint16_t LCD_LIST_TXT_len(char* bufTxt, TEXT_ARRANGEMENT arangType, int fontID,int space,int constWidth, uint16_t* lenMaxStrips);
 
 char* LCD_LIST_TXT_example(char* buf);
+uint16_t LCD_LIST_TXT_nmbStripsInLine(GET_SET act, char* bufTxt, int* lenBufTxt);
+uint16_t LCD_LIST_TXT_lenLineInPxl(char* bufTxt, TEXT_ARRANGEMENT arangType, int fontID,int space,int constWidth, uint16_t* lenMaxStrips);
 StructTxtPxlLen LCD_ListTxtWin(uint32_t posBuff,uint32_t BkpSizeX,uint32_t BkpSizeY,int fontID, int Xpos, int Ypos, char *txt, int OnlyDigits, int space, uint32_t bkColor, uint32_t fontColor,uint8_t maxVal, int constWidth, uint32_t fontColorTab[], TEXT_ARRANGEMENT txtSeqRow);
 
 void LCD_ResetStrMovBuffPos(void);
