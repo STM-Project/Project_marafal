6e0732168d7026c913232f95275e70ceaf849194
marafal <marafal@interia.pl>
2024-12-14 17:41:46 +0100

    HTTPS  CLEANED !!!!!!!!  :)

--- MBEDTLS/App/mbedtls.c
+++ MBEDTLS/App/mbedtls.c
@@ -30,22 +30,18 @@
 /* USER CODE END 0 */
 
 /* USER CODE BEGIN 1 */
-#if defined(MBEDTLS_SSL_CACHE_C)
-#include "mbedtls/ssl_cache.h"
-#endif
-#include "string.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "sys.h"
-#include "platform.h"
 #include "memory_buffer_alloc.h"
 #if defined(MBEDTLS_SSL_CACHE_C)
 #include "mbedtls/ssl_cache.h"
 #endif
-#include "string.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "sys.h"
 #include "ff.h"
 #include "sd_card.h"
 #include "timer.h"
+#include "string.h"
+#include "string_oper.h"
 /* USER CODE END 1 */
 
 /* Global variables ---------------------------------------------------------*/
@@ -56,7 +52,12 @@ mbedtls_ctr_drbg_context ctr_drbg;
 mbedtls_entropy_context entropy;
 
 /* USER CODE BEGIN 2 */
+#define HTTPS_DEBUG	1
 #define HTTPS_MAX_WRITE_BUFF	16384
+
+#if HTTPS_MAX_WRITE_BUFF > MBEDTLS_SSL_MAX_CONTENT_LEN
+#error "Write buffer size NOT large as MBEDTLS_SSL_MAX_CONTENT_LEN"
+#endif
 
 extern void Dbg(int on, char *txt);
 extern char* GETVAL_ptr();
@@ -70,8 +71,8 @@ static const uint8_t *pers = (uint8_t*) "ssl_server";
  mbedtls_ssl_cache_context cache;
 #endif
 
-static char buf[2400];
 unsigned char memory_buf[4*HTTPS_MAX_WRITE_BUFF];
+static char buffRecv[110];
 
 static sys_thread_t  vTaskHandleServer;
 /* USER CODE END 2 */
@@ -103,6 +104,14 @@ static int HTTPS_send(mbedtls_ssl_context *ssl, char *data, size_t len){
 	}
 	return 0;
 }
+static int HTTPS_recv(mbedtls_ssl_context *ssl, char *data, size_t len){
+	int ret = mbedtls_ssl_read(ssl, (unsigned char*)data, len);
+   if(ret <= 0)	/* MBEDTLS_ERR_SSL_PEER_CLOSE_NOTIFY,	MBEDTLS_ERR_NET_CONN_RESET,	MBEDTLS_ERR_SSL_WANT_READ,	  MBEDTLS_ERR_SSL_WANT_WRITE,	  ... */
+   	return 1;
+   else if(ret < 5)
+   	return 2;
+   return 0;
+}
 
 static void HTTPS_close(void){
 	mbedtls_net_free(&client_fd);
@@ -120,9 +129,9 @@ static void HTTPS_close(void){
 
 static void SSL_Server(void *arg)
 {
-	int ret;
+	int ret,len;
 
-	START__SSL_Server:
+	/* START__SSL_Server: */
 	#ifdef MBEDTLS_MEMORY_BUFFER_ALLOC_C
 		mbedtls_memory_buffer_alloc_init(memory_buf, sizeof(memory_buf));
 	#endif
@@ -166,118 +175,112 @@ static void SSL_Server(void *arg)
 	mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
 
 
-#if defined(MBEDTLS_SSL_CACHE_C)
-	mbedtls_ssl_conf_session_cache(&conf, &cache, mbedtls_ssl_cache_get, mbedtls_ssl_cache_set);
-#endif
+	#if defined(MBEDTLS_SSL_CACHE_C)
+		mbedtls_ssl_conf_session_cache(&conf, &cache, mbedtls_ssl_cache_get, mbedtls_ssl_cache_set);
+	#endif
 
-		mbedtls_ssl_conf_ca_chain(&conf, srvcert.next, NULL);
+	mbedtls_ssl_conf_ca_chain(&conf, srvcert.next, NULL);
 
-		ret = mbedtls_ssl_conf_own_cert(&conf, &srvcert, &pkey);
-		if(ret != 0)
-			goto exit;
+	ret = mbedtls_ssl_conf_own_cert(&conf, &srvcert, &pkey);
+	if(ret != 0)
+		goto exit;
 
-		ret = mbedtls_ssl_setup(&ssl, &conf);
-		if(ret != 0)
-			goto exit;
+	ret = mbedtls_ssl_setup(&ssl, &conf);
+	if(ret != 0)
+		goto exit;
 
-		mbedtls_ssl_conf_read_timeout( &conf, 2000 );
+	mbedtls_ssl_conf_read_timeout(&conf, 2000);
 
-  do{
+	do
+	{
+		RESET_Connection:
+		mbedtls_net_free(&client_fd);
+		mbedtls_ssl_session_reset(&ssl);
 
-		startt:
-
-			mbedtls_net_free(&client_fd);
-			mbedtls_ssl_session_reset(&ssl);
-
-
-
-		ret = mbedtls_net_accept(&listen_fd, &client_fd, NULL, 0, NULL);  //To TU OCZEKUJE !!!
-		if (ret != 0){
-			if(ret == MBEDTLS_ERR_NET_ACCEPT_FAILED){
-				goto startt;
-			}
-			else
-				goto startt;
-		}
-
+		ret = mbedtls_net_accept(&listen_fd, &client_fd, NULL, 0, NULL);		/* wait for connection */
+		if (ret != 0)
+			goto RESET_Connection;
 
 		mbedtls_ssl_set_bio(&ssl, &client_fd, mbedtls_net_send, mbedtls_net_recv, mbedtls_net_recv_timeout);
 
-
-			while ((ret = mbedtls_ssl_handshake(&ssl)) != 0)
-			{
-				if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE)
-					goto startt;
-			}
-
-
-		int len = sizeof(buf) - 1;
-		memset(buf, 0, sizeof(buf));
-
-		ret = mbedtls_ssl_read(&ssl, (unsigned char*) buf, len); /*buf[30]=0;*/
-//		if (ret == MBEDTLS_ERR_NET_CONN_RESET || (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE))
-//			goto exit;
-
-
-		if(strstr(buf, "GET / "))  //if ((len >= 5) && (strncmp(buf, "GET /", 5) == 0))
+		while ((ret = mbedtls_ssl_handshake(&ssl)) != 0)
 		{
+			if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE)
+				goto RESET_Connection;
+		}
 
-			  if(TakeMutex2(Semphr_sdram,Semphr_cardSD,1000))
-			  {
-				  SDCardFileOpen(0,"aaa.htm",FA_READ);
-				  int len = SDCardFileRead(0, GETVAL_ptr(0), 180000);
-				  SDCardFileClose(0);
+		len = sizeof(buffRecv);
+		memset(buffRecv, 0, len);
+		if(HTTPS_recv(&ssl, buffRecv, len-1))
+			goto RESET_Connection;
 
-				  int count=0;
-				  while(1)
-				  {
-					  if(len < HTTPS_MAX_WRITE_BUFF)  //MBEDTLS_SSL_OUT_CONTENT_LEN
-					  {
-						  if(HTTPS_send(&ssl,GETVAL_ptr(count),len)){	GiveMutex2(Semphr_sdram,Semphr_cardSD);
-							  goto startt;	}
-							break;
-					  }
-					  else
-					  {
-						  if(HTTPS_send(&ssl,GETVAL_ptr(count),HTTPS_MAX_WRITE_BUFF)){	GiveMutex2(Semphr_sdram,Semphr_cardSD);
-							  goto startt;	}
+		if(0==strncmp(buffRecv, "GET / ...", 6))		/* strstr(buffRecv, "GET / ") */
+		{
+			if(TakeMutex2(Semphr_sdram,Semphr_cardSD,1000))
+			{
+				int count=0, len;
+				SDCardFileOpen(0,"aaa.htm",FA_READ);
+				len = SDCardFileRead(0, GETVAL_ptr(0), 180000);
+				SDCardFileClose(0);
 
-							count += HTTPS_MAX_WRITE_BUFF;
-							len -= HTTPS_MAX_WRITE_BUFF;
-					  }
-				  }
-				  GiveMutex2(Semphr_sdram,Semphr_cardSD);
-				  Dbg(1,"\r\nGET_GET_GET...");
+				while(1)
+				{
+					if(len < HTTPS_MAX_WRITE_BUFF)
+					{
+						if(HTTPS_send(&ssl,GETVAL_ptr(count),len)){	GiveMutex2(Semphr_sdram,Semphr_cardSD);
+							goto RESET_Connection;	}
+						break;
+					}
+					else
+					{
+						if(HTTPS_send(&ssl,GETVAL_ptr(count),HTTPS_MAX_WRITE_BUFF)){	GiveMutex2(Semphr_sdram,Semphr_cardSD);
+							goto RESET_Connection;	}
 
-			  }
-    }
-	 else
-	 {
-		  if(HTTPS_send(&ssl,(char*)"1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890",100)){	GiveMutex2(Semphr_sdram,Semphr_cardSD);
-			  goto startt;	}
+						count += HTTPS_MAX_WRITE_BUFF;
+						len -= HTTPS_MAX_WRITE_BUFF;
+					}
+				}
+				GiveMutex2(Semphr_sdram,Semphr_cardSD);
+				Dbg(HTTPS_DEBUG,"\r\nGET...");
+			}
+		}
+		else if(0==strncmp(buffRecv, "GET /test", 9))
+		{
+			if(HTTPS_send(&ssl,"<html><body>Testttt</body></html>",33))
+				goto RESET_Connection;
+			Dbg(HTTPS_DEBUG,"j");
+		}
+		else
+		{
+			static int testVar=0;
+			char *ptr = Int2Str(++testVar,Zero,6,Sign_none);
 
-		 Dbg(1,"i");
-	 }
+			strcpy(buffRecv,"1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
+			buffRecv[0] = *(ptr+0);
+			buffRecv[1] = *(ptr+1);
+			buffRecv[2] = *(ptr+2);
+			buffRecv[3] = *(ptr+3);
+			buffRecv[4] = *(ptr+4);
+			buffRecv[5] = *(ptr+5);
+			buffRecv[6] = ' ';
 
+			if(HTTPS_send(&ssl,buffRecv,100))
+				goto RESET_Connection;
 
-
-//#define XXXXX_DD 333
-//
-//#if BUFFER_LEN > MBEDTLS_SSL_MAX_CONTENT_LEN
-//#error "adsfsdf"
-//#endif
-
-
+			Dbg(HTTPS_DEBUG,"i");
+		}
 
 	  
+
+
 		while ((ret = mbedtls_ssl_close_notify(&ssl)) < 0)
 		{
 			if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE)
-				goto startt;
+				goto RESET_Connection;
 		}
 
 
-  }while(1);
+	}while(1);
 
 
 	exit:
