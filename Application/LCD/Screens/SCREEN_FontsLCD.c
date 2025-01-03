7a23d5a0437d7fbd27edfd3579b236c51742f50c
STM-Project <51048656+STM-Project@users.noreply.github.com>
2025-01-03 16:31:08 +0100

    Update SCREEN_FontsLCD.c

--- Application/LCD/Screens/SCREEN_FontsLCD.c
+++ Application/LCD/Screens/SCREEN_FontsLCD.c
@@ -2260,6 +2260,23 @@ void FILE_NAME(main)(int argNmb, char **argVal)
 	LCD_Rectangle2____(0, LCD_X,LCD_Y, 20,300, 100, 40,  0xFF202020,0xFF282828,   v.COLOR_FillFrame,BrightDecr(v.COLOR_FillFrame,0x95),  v.COLOR_BkScreen, 0.0, Middle);
 	LCD_Rectangle2____(0, LCD_X,LCD_Y, 160,300, 100, 40, 0xFF202020,0xFF282828,   v.COLOR_FillFrame,BrightDecr(v.COLOR_FillFrame,0x95),  v.COLOR_BkScreen, 0.0, Middle2);
 
+
+	LCD_RoundRectangle(0, LCD_X,LCD_Y, 270,300, 100, 40,   0xFFF0F0F0,    0xFFF0F0F0,  			 v.COLOR_BkScreen);
+	LCD_RoundRectangle(0, LCD_X,LCD_Y, 270,302, 100, 40,   v.COLOR_Frame,  v.COLOR_FillFrame,  v.COLOR_BkScreen);
+
+	LCD_RoundRectangle(0, LCD_X,LCD_Y, 270,400, 100, 40,   0xFF202020,    0xFF202020,  			 v.COLOR_BkScreen);
+	LCD_RoundRectangle(0, LCD_X,LCD_Y, 270,398, 100, 40,   v.COLOR_Frame,  v.COLOR_FillFrame,  v.COLOR_BkScreen);
+
+
+
+	LCD_LittleRoundRectangle(0, LCD_X,LCD_Y, 570,240, 100, 40,   0xFFF0F0F0,    0xFFF0F0F0,  			 v.COLOR_BkScreen);
+	LCD_LittleRoundRectangle(0, LCD_X,LCD_Y, 570,242, 100, 40,   v.COLOR_Frame,  v.COLOR_FillFrame,  v.COLOR_BkScreen);
+
+	LCD_LittleRoundRectangle(0, LCD_X,LCD_Y, 570,300, 100, 40,   0xFF202020,    0xFF202020,  			 v.COLOR_BkScreen);
+	LCD_LittleRoundRectangle(0, LCD_X,LCD_Y, 570,298, 100, 40,   v.COLOR_Frame,  v.COLOR_FillFrame,  v.COLOR_BkScreen);
+
+
+
 	if(LoadWholeScreen  == argNmb) TxtTouch(TouchSetNew);
 	if(LoadNoDispScreen != argNmb) LCD_Show();
 
