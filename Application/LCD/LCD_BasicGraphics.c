5a9ca76df66ddfe14374a0b22b33c9c4c8d611af
marafal <marafal@interia.pl>
2025-04-12 16:05:13 +0200

    xcvxcv

--- Application/LCD/LCD_BasicGraphics.c
+++ Application/LCD/LCD_BasicGraphics.c
@@ -5467,16 +5467,27 @@ void GRAPH_GetSamplesAndDraw(structRepPos posXY_rep[], int startX,int startY, in
 
 	int testAAAAA = testFuncGraph;
 
+	u32 ccccol=0;
+	u32 bkColor=0;
+	float transCoeff=0.0;
+	u32 xxxx=0;
 
+	if(startX < 100) ccccol = LIGHTYELLOW;
+	if(startX > 100) ccccol = RED;
 //funkcja znajdujaca max wykresu do ograniczenia !!!!!!!!!!!!!!!!!
 
 	LOOP_FOR(i,len_posXY){
 		//if(pLcd[LCD_X*(posXY[i].y+1)+posXY[i].x] != colorLineAA){
-		if(_PLCD(posXY[i].x, posXY[i].y) != colorLineAA){
-			for(int j=posXY[i].y; j<startY+yMax; ++j){   //LOOP_INIT(j,posXY[i].y+1,startY+yMax)
-				//pLcd[LCD_X*j+posXY[i].x] = GetTransitionColor(0xCCCCCC/*colorLineAA*/, 0x383838, (1.2*((float)j))/(float)(LCD_Y) + 0.1 );  //tablocowanie zrobic bo zawolno !!!! zeby nie obliczac za kazdym razem i szybciej przez to !!!
-				_PLCD(posXY[i].x, j) = GetTransitionColor(YELLOW, 0x383838, (1.00*((float)(j-(startY+yMin))))/(float)(LCD_Y-(startY+yMin)) + 0.30 );
+		if(_PLCD(posXY[i].x, posXY[i].y+1) != colorLineAA  &&  _PLCD(posXY[i].x, posXY[i].y+1) != LIGHTBLUE){
+			if(posXY[i].x != xxxx){
+				for(int j=posXY[i].y+1; j<startY+yMax; ++j){   //LOOP_INIT(j,posXY[i].y+1,startY+yMax)
+					//pLcd[LCD_X*j+posXY[i].x] = GetTransitionColor(0xCCCCCC/*colorLineAA*/, 0x383838, (1.2*((float)j))/(float)(LCD_Y) + 0.1 );  //tablocowanie zrobic bo zawolno !!!! zeby nie obliczac za kazdym razem i szybciej przez to !!!
+					transCoeff = (1.00*((float)(j-(startY+yMin))))/(float)(LCD_Y-(startY+yMin)) + 0.15;
+					bkColor = _PLCD(posXY[i].x, j);
+					_PLCD(posXY[i].x, j) = GetTransitionColor(ccccol,bkColor, transCoeff/*(1.00*((float)(j)))/(float)(LCD_Y) + 0.15*/ );
+				}
 			}
+			xxxx=posXY[i].x;
 		}  //(0.5*((float)j-(posXY[i].y+1)))/(float)(LCD_Y-5-(posXY[i].y+1)) + 0.5   //rowne gradienty od lini
 	}
 
