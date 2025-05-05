772beb826b83d48eb0ba7dd9b043119dedff0a73
STM-Project <51048656+STM-Project@users.noreply.github.com>
2025-05-05 15:56:37 +0200

    Update examples.h

--- Application/examples.h
+++ Application/examples.h
@@ -8,13 +8,13 @@
 #ifndef LCD_SCREENS_EXAMPLES_H_
 #define LCD_SCREENS_EXAMPLES_H_
 
-void LCDEXAMPLE_RectangleGradient(u32 COLOR_FillFrame, u32 COLOR_Frame, u32 COLOR_BkScreen, int FONT_ID_Descr);
-void LCDEXAMPLE_GradientCircleButtonAndSlider(int FONT_ID_Title, int FONT_VAR_Title, u32 COLOR_FillMainFrame, u32 COLOR_Frame, u32 COLOR_BkScreen);
-void LCDEXAMPLE_LcdTxt(int FONT_ID_Fonts, int FONT_VAR_Fonts, u32 COLOR_FillMainFrame, u32 COLOR_Frame, u32 COLOR_BkScreen);
-void LCDEXAMPLE_DrawLine(u32 COLOR_Frame, u32 COLOR_BkScreen);
+void LCDEXAMPLE_RectangleGradient					(u32 COLOR_FillFrame, u32 COLOR_Frame, u32 COLOR_BkScreen, int FONT_ID_Descr);
+void LCDEXAMPLE_GradientCircleButtonAndSlider	(int FONT_ID_Title, int FONT_VAR_Title, u32 COLOR_FillMainFrame, u32 COLOR_Frame, u32 COLOR_BkScreen);
+void LCDEXAMPLE_LcdTxt									(int FONT_ID_Fonts, int FONT_VAR_Fonts, u32 COLOR_FillMainFrame, u32 COLOR_Frame, u32 COLOR_BkScreen);
+void LCDEXAMPLE_DrawLine								(u32 COLOR_Frame, u32 COLOR_BkScreen);
+void LCDEXAMPLE_Graph									(void);
 
-void LCDEXAMPLE_Graph(void);
-
-void MEMEXAMPLE_MemoryFunc(void);
+void MEMEXAMPLE_MemoryFunc								(void);
+void DBGEXAMPLE_Graph									(void);
 
 #endif /* LCD_SCREENS_EXAMPLES_H_ */
