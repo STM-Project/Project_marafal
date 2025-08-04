0fbe11db7bbc60ecfe2821c69bbe6ba6181196c1
STM-Project <51048656+STM-Project@users.noreply.github.com>
2025-08-04 15:52:31 +0200

    Update examples.h

--- Application/examples.h
+++ Application/examples.h
@@ -12,7 +12,7 @@ void LCDEXAMPLE_RectangleGradient					(u32 COLOR_FillFrame, u32 COLOR_Frame, u32
 void LCDEXAMPLE_GradientCircleButtonAndSlider	(int FONT_ID_Title, int FONT_VAR_Title, u32 COLOR_FillMainFrame, u32 COLOR_Frame, u32 COLOR_BkScreen);
 void LCDEXAMPLE_LcdTxt									(int FONT_ID_Fonts, int FONT_VAR_Fonts, u32 COLOR_FillMainFrame, u32 COLOR_Frame, u32 COLOR_BkScreen);
 void LCDEXAMPLE_DrawLine								(u32 COLOR_Frame, u32 COLOR_BkScreen);
-void LCDEXAMPLE_Graph									(USER_GRAPH_PARAM graph, int touchStart, int FONT_ID_Descr);
+void LCDEXAMPLE_Graph									(int displayOption, USER_GRAPH_PARAM graph, int touchStart, int FONT_ID_Descr);
 void LCDEXAMPLE_CreateLoadDisplayFonts				(u32 COLOR_BkScreen);
 
 void MEMEXAMPLE_MemoryFunc								(void);
