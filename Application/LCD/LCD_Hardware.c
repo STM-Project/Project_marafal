d51fd035edf3f79e1d9b080acc0f8cdc6a6dae33
STM-Project <51048656+STM-Project@users.noreply.github.com>
2025-08-27 09:57:54 +0200

    Update LCD_Hardware.c

--- Application/LCD/LCD_Hardware.c
+++ Application/LCD/LCD_Hardware.c
@@ -58,8 +58,14 @@ void LCD_DisplayBuff(uint32_t Xpos, uint32_t Ypos, uint32_t width, uint32_t heig
 	{
 		while(HAL_DMA2D_STATE_READY!=HAL_DMA2D_GetState(&hdma2d));
 		LCD_SetOutputOffset(width);
-	/* SCB_InvalidateDCache_by_Addr(pbmp, width*height*sizeof(uint32_t)); */
-		SCB_CleanDCache_by_Addr(pbmp, width*height*sizeof(uint32_t));
+
+		uintptr_t start = (uintptr_t) pbmp;
+  		size_t 	 len 			  = (size_t) ( width*height*sizeof(uint32_t) );
+  		uintptr_t alignedStart = start & ~(uintptr_t) 31U;
+  		size_t 	 alignedLen   = (size_t) ((start - alignedStart) + len + 31U) & ~(size_t) 31U;
+  		SCB_CleanDCache_by_Addr((uint32_t*)alignedStart, (int32_t)alignedLen);
+
+		//SCB_CleanDCache_by_Addr(pbmp, width*height*sizeof(uint32_t));
 		HAL_DMA2D_Start_IT(&hdma2d, (uint32_t)pbmp, LCD_GetPositionAddress(Xpos,Ypos), width, height);
 		while(HAL_DMA2D_STATE_READY!=HAL_DMA2D_GetState(&hdma2d));
 
