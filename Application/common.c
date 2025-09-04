f8ad1ae2e762c23ec068dfc507ad548e5b57e776
STM-Project <51048656+STM-Project@users.noreply.github.com>
2025-09-04 14:06:22 +0200

    Update common.c

--- Application/common.c
+++ Application/common.c
@@ -173,3 +173,14 @@ int COMPARE_2Struct(void *struct1, void *struct2, int structSize, DATA_TYPE data
 	#undef _OPERAT
 }
 
+void SORT_Bubble(int tab[], int n){
+	int i, j, temp;
+	for (i = 0; i < n - 1; i++) {
+		for (j = 0; j < n - i - 1; j++) {
+			if (tab[j] > tab[j + 1]) {
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+	}}}
+}
+
