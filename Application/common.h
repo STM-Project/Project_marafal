d0d94dc9e0bdab9b0ae6b5403a66d6a662479803
STM-Project <51048656+STM-Project@users.noreply.github.com>
2025-09-22 15:15:06 +0200

    Update common.h

--- Application/common.h
+++ Application/common.h
@@ -303,6 +303,7 @@ int FV2(char* descr, VARIABLE_ACTIONS type, int nrMem, int val);
 struct_MATH CALCULATE_MinMaxAvr (GET_SET operType, int nr, void *value, DATA_TYPE dataType);
 int 			COMPARE_2Struct	  (void *struct1, void *struct2, int structSize, DATA_TYPE dataType);
 void 			SORT_Bubble			  (int tab[], int n);
+void 			AVR_Calc				  (int numbersOfSamples, int *pSamplesIn, int *pSamplesOut, int coeff);
 
 
 
