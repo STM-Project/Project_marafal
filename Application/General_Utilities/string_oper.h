f139b3a84725fcbc27eab00f7c82130d5df0315d
STM-Project <51048656+STM-Project@users.noreply.github.com>
2026-02-09 14:45:09 +0100

    Update string_oper.h

--- Application/General_Utilities/string_oper.h
+++ Application/General_Utilities/string_oper.h
@@ -12,6 +12,8 @@
 #define _MILLION		1000000
 #define _THOUSAND		1000
 
+#define IPDOT(a,b,c,d)	((a<<24)|(b<<16)|(c<<8)|(d))
+
 typedef enum{
 	SetVal,
 	GetVal,
@@ -40,5 +42,12 @@ void SwapUint16(uint16_t *a, uint16_t *b);
 int STRING_GetTheLongestTxt(int nmb, char **txt);
 int STRING_CmpTxt(char* src, char* dst);
 char* DispLongNmb(uint32_t nmb, char* bufStr);
+
+void StrBuffCopy(char *dest, char *src);
+void StrBuffCopylimit(char *dest, char *src, int lenSrc);
+
+uint32_t IPStr2Int(char *str);
+uint64_t MACStr2Int64(char *str);
+char* IP2Str(uint32_t value);
 
 #endif /* GENERAL_UTILITIES_STRING_OPER_H_ */
