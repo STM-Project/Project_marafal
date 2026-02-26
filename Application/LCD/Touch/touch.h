303ffaa4f9afd51806744be25c32b4d891f88855
STM-Project <51048656+STM-Project@users.noreply.github.com>
2026-02-26 15:53:39 +0100

    Update touch.h

--- Application/LCD/Touch/touch.h
+++ Application/LCD/Touch/touch.h
@@ -29,6 +29,45 @@ typedef enum{
 #define CLR_ALL_TOUCH 							for(int i=0;i<SEL_BITS_SIZE;++i) FILE_NAME(SelTouch)[i]=0
 #define GET_TOUCH 								FILE_NAME(SelTouch)[0]!=0 || FILE_NAME(SelTouch)[1]!=0 || FILE_NAME(SelTouch)[2]!=0 || FILE_NAME(SelTouch)[3]!=0 || FILE_NAME(SelTouch)[4]!=0		/* determine by 'SEL_BITS_SIZE' */
 
+#define CASE_TOUCH_STATE(screenTouchState,touchPoint, src,dst, txt,coeff, touchX, touchX2) \
+	case touchPoint:\
+	if(NotServiceTouchAboveWhenWasClearedThis(touchX) & NotServiceTouchAboveWhenWasClearedThis(touchX2)){\
+		if(0==CHECK_TOUCH(screenTouchState)){\
+			if(GET_TOUCH){ FILE_NAME(main)(LoadPartScreen,(char**)ppMain); CLR_ALL_TOUCH; }\
+			SELECT_CURRENT_FONT(src, dst, txt, coeff);\
+			SET_TOUCH(screenTouchState);\
+			SetTouchFlag();\
+		}\
+		else{\
+			FILE_NAME(main)(LoadPartScreen,(char**)ppMain);\
+			KEYBOARD_TYPE(KEYBOARD_none,0);\
+			CLR_TOUCH(screenTouchState);\
+		}}
+
+#define TOUCH_FUNCTIONS_ \
+	int NotServiceTouchAboveWhenWasClearedThis(TOUCH_POINTS touch){ \
+		return CONDITION(NoTouch==touch, 1, !CHECK_TOUCH(touch) && !_WasState(touch)); \
+	} \
+	void _TouchService(TOUCH_POINTS touchStart,TOUCH_POINTS touchStop, KEYBOARD_TYPES keyboard, SELECT_PRESS_BLOCK releaseAll,SELECT_PRESS_BLOCK keyStart, TOUCH_FUNC *func){ \
+		if(IS_RANGE(screenTouchState, touchStart, touchStop)){ \
+			int nr = screenTouchState-touchStart; \
+			if(releaseAll){  if(_WasStatePrev(touchStart,touchStop)) KEYBOARD_TYPE(keyboard,releaseAll);  } \
+			if(func) func(nr); \
+			if(KEY_Select_one==keyStart) nr=0; \
+			KEYBOARD_TYPE_PARAM(keyboard,keyStart+nr,screenTouchPos.x,screenTouchPos.y,0,0,0); _SaveState(); \
+	}} \
+	void _TouchEndService(TOUCH_POINTS touchStart,TOUCH_POINTS touchStop, KEYBOARD_TYPES keyboard, SELECT_PRESS_BLOCK releaseAll, TOUCH_FUNC *func){ \
+		if(_WasStateRange(touchStart, touchStop)){ \
+			KEYBOARD_TYPE(keyboard, releaseAll); \
+			if(func) func(-1); \
+	}} \
+	int _KEYBOARD_setTxt__SERVICE(u16 screenTouchState,int touchStart,int touchStop, int keyStart){ \
+			  if( IS_RANGE(screenTouchState,touchStart,touchStop))								{	if(_WasStatePrev(touchStart,touchStop)) KEYBOARD_TYPE(KEYBOARD_setTxt,KEY_All_release);								 KEYBOARD_TYPE(KEYBOARD_setTxt, keyStart+(screenTouchState-touchStart));  _SaveState();	return 1;  } \
+		else if(_WasStateRange(Touch_exit,Touch_exit) && _SET==LCDTOUCH_UserStatus(_GET)){	LCD_TOUCH_RestoreAllSusspendedTouchs(); ServiceKeyCharBuff(); 	FILE_NAME(main)(LoadPartScreen,(char**)ppMain);	 KEYBOARD_TYPE(KEYBOARD_none,0);	 																			return 1;  } \
+		else if(_WasStateRange(touchStart,touchStop))									 			{																																						 KEYBOARD_TYPE(KEYBOARD_setTxt, KEY_All_release);  													return 1;  } \
+		return 0; \
+	}
+
 enum TOUCH_TYPE{
   ID_TOUCH_POINT,
   ID_TOUCH_POINT_WITH_HOLD,				/* if press at least specific time */
@@ -65,7 +104,6 @@ extern XY_Touch_Struct  structXY_Zero;
 
 extern XY_Touch_Struct screenTouchPos;
 extern uint16_t 		  screenTouchState;
-extern uint16_t 		  screenTouchStatePrev, screenTouchStatePrev2;
 
 void		LCD_TOUCH_Service					(void);
 uint16_t LCD_TOUCH_GetTypeAndPosition	(XY_Touch_Struct *posXY);
@@ -123,13 +161,18 @@ int LCDTOUCH_UserStatus(int nr);
 void SetTouchFlag		(void);
 int  IsSetTouchFlag	(void);
 void TouchScreenInit (void);
+void TOUCHSCREEN_Exit(void);
+
 void _SaveState (void);
 /*	void _RstState	 (void); */
 void _SaveState2(void);
 void _RstState2 (void);
-int _WasState(int point);
+int _WasState	 (int point);
 int _WasStateRange(int point1, int point2);
 int _WasStatePrev(int rangeMin,int rangeMax);
+
+void _RestoreSusspendedTouchsByAnotherClickItem(int prev,int prevStart,int prevStop, 	int not1,int not2,int not3,int not4,int not5,int not6,int not7,int not8,int not9,int not10, 		int unblock1,int unblock2,int unblock3,int unblock4,int unblock5,int unblock6,int unblock7,int unblock8,int unblock9,int unblock10);
+int NotServiceTouchAboveWhenWasClearedThis(int touch);
 
 //void _RestoreSusspendedTouchsByAnotherClickItem___(int state,int statePrev2, int prev,int prevStart,int prevStop, 	int not1,int not2,int not3,int not4,int not5,int not6,int not7,int not8,int not9,int not10, 		int unblock1,int unblock2,int unblock3,int unblock4,int unblock5,int unblock6,int unblock7,int unblock8,int unblock9,int unblock10);
 
