1c0dc6f741a7a7182fdaf58d45e18ba41f37e2a1
STM-Project <51048656+STM-Project@users.noreply.github.com>
2026-03-16 08:56:37 +0100

    Update double_float.h

--- Application/General_Utilities/double_float.h
+++ Application/General_Utilities/double_float.h
@@ -8,7 +8,7 @@
 #ifndef GENERAL_UTILITIES_DOUBLE_FLOAT_H_
 #define GENERAL_UTILITIES_DOUBLE_FLOAT_H_
 
-void  dbl2stri			  (char *buffer, double dbl, unsigned int dec_digits);
+char* dbl2stri		  	  (char *buffer, double dbl, unsigned int dec_digits);
 void  float2stri		  (char *buffer, float dbl, unsigned int dec_digits);
 float GetNewfloatValue (float value, int precision);
 
